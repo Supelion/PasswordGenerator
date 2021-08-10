19d461661e32bef255b74e8b2c4e5638bda31cd3
Supelion <83178953+Supelion@users.noreply.github.com>
2021-08-10 09:50:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 import random
-import os
-import ctypes
-
-os.system("cls")
-ctypes.windll.kernel32.SetConsoleTitleW("Password Generator | Supelion")
 
 lettersAndOtherThings = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', "A", "B", "C", "D", 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', "W", 'X', 'Y', 'Z']
 numbers = ["1", "2", "3", "4", "5", "6", "8", "8", "9", "0"]
